070514bc67da2b5ba1708af3a2115d81f056e313
thuvejan <thuvejan@users.noreply.github.com>
2016-09-21 15:49:07 -0400

    added code to reduce image quality, which will reduce URI size

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var fileInput = document.getElementById("input-img");
 var imgUploadLink = document.getElementById("img-click-upload");
 var img = document.getElementById("img-view");
+var config = (function () {
+    var obj = {}
+    obj.MAX_URI_LENGTH = 32000;
+    obj.URI_RANGE_ALLOWANCE = 250;
+    return obj;
+}());
 
 function hashImage() {
     var fr = new FileReader();
@@ -63,3 +69,55 @@ img.addEventListener("load", function() {
     img.setAttribute("width", x);
     img.setAttribute("height", y);
 }, false);
+
+/**
+ * Reduce the quality, and subsequently, the size of the uploaded image.
+ * 
+ * @param {File} imgFile  - The image submitted by the user.
+ * @returns {string} imgUri - The data URI of imgFile.
+ */
+function reduceQuality(imgFile) {
+    debugger;
+    var img = new Image();
+    if (window.URL) {
+        img.src = URL.createObjectURL(imgFile);
+    } else if (window.webkitURL) {
+        img.src = webkitURL.createObjectURL(imgFile);
+    }
+
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    var min = 0, max = 1, mid = 0.5;
+    var imgUri = canvas.toDataURL(imgFile.type, max);
+    var doCompress = imgUri.length > config.MAX_URI_LENGTH ?
+        inRange(imgUri.length, config.URI_RANGE_ALLOWANCE, config.MAX_URI_LENGTH) :
+        false;
+
+    while (doCompress) {
+        if (Math.min(imgUri.length, config.MAX_URI_LENGTH) == imgUri.length) {
+            min = mid;
+        } else if (Math.max(imgUri.length, config.MAX_URI_LENGTH) == imgUri.length) {
+            max = mid;
+        }
+        mid = (max + min) / 2;
+        imgUri = canvas.toDataURL(imgFile.type, mid);
+    }
+    return imgUri;
+}
+
+/**
+ * Checks whether a number num is within plus/minus k integers of x. i.e. Is
+ * num within the range [x-k, x+k]?
+ * 
+ * @param {Number} num - The number to 
+ * @param {Number} k - 
+ * @param {Number} x - The 
+ * @returns {boolean} - Is num in the range [x-k, x+k]?
+ */
+function inRange(num, k, x) {
+    if ((num <= x - k) || (num > x + k)) {
+        return true;
+    }
+    return false
+}
